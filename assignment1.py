ad4b7ab6cc886262daca770e416bb414de651a25
Khaveesh N <khaveesh@gmail.com>
2020-08-16 23:30:20 +0530

    Added multiplication

--- assignment1.py
+++ assignment1.py
@@ -1,20 +1,26 @@
-def addition(a,b):
-	return a+b
+def addition(a, b):
+    return a + b
 
-def subtraction(a,b):
-	return a-b;
+
+def subtraction(a, b):
+    return a - b
+
+
+def multiplication(a, b):
+    return a * b
 
 
 if __name__ == "__main__":
-	a = 3
-	b = 5
-	print(addition(a,b))
-	print(subtraction(a,b))
+    a = 3
+    b = 5
+    print(addition(a, b))
+    print(subtraction(a, b))
+    print(multiplication(a, b))
 
 
-'''
+"""
 clone from github
 Create new branches for Multiplication and Division
 merge with master
-push to github 
-'''
+push to github
+"""
